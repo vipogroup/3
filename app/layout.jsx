1edbf96220be84ae7dd68539965fbf2fe30a3f25
vipogroup <dev@vipogroup.local>
2026-01-02 17:51:04 +0200

    fix: Add mobile-web-app-capable meta tag

--- app/layout.jsx
+++ app/layout.jsx
@@ -42,6 +42,7 @@ export default function RootLayout({ children }) {
         <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="192x192" href="/icons/192.png" />
         <link rel="icon" type="image/svg+xml" href="/icons/vipo-icon.svg" />
+        <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content="VIPO" />
